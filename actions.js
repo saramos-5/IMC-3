7984329f6b4fed21f5dbf2b89946348dcd89efdc
saramos-5 <saramos5@miners.utep.edu>
2023-04-12 00:17:33 -0600

    js for screen changes

---
+++ actions.js
@@ -0,0 +1,9 @@
+function to_export(){
+    window.location.href = "export.html"
+}
+function to_home(){
+    window.location.href = "index.html"
+}
+function to_exit(){
+    window.location.href = "exit.html"
+}
